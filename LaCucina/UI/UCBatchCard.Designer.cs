fff00936690437d3ec661e70a1f002f6812eb6c4
AmaniALiBen <ahnaaama@gmail.com>
2026-05-28 13:24:05 +0200

    الكرت قبل التعديل

---
+++ LaCucina/UI/UCBatchCard.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace LaCucina.UI
+{
+    partial class UCBatchCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.smoothFlowPanel1 = new SmoothFlowPanel();
+            this.ucItemInBatch1 = new LaCucina.UI.UCItemInBatch();
+            this.ucItemInBatch2 = new LaCucina.UI.UCItemInBatch();
+            this.ucItemInBatch3 = new LaCucina.UI.UCItemInBatch();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.smoothFlowPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSize = true;
+            this.panel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.panel3.Controls.Add(this.smoothFlowPanel1);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.MinimumSize = new System.Drawing.Size(375, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.panel3.Size = new System.Drawing.Size(395, 724);
+            this.panel3.TabIndex = 3;
+            // 
+            // smoothFlowPanel1
+            // 
+            this.smoothFlowPanel1.AutoScroll = true;
+            this.smoothFlowPanel1.AutoSize = true;
+            this.smoothFlowPanel1.Controls.Add(this.ucItemInBatch1);
+            this.smoothFlowPanel1.Controls.Add(this.ucItemInBatch2);
+            this.smoothFlowPanel1.Controls.Add(this.ucItemInBatch3);
+            this.smoothFlowPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.smoothFlowPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.smoothFlowPanel1.Location = new System.Drawing.Point(0, 76);
+            this.smoothFlowPanel1.MinimumSize = new System.Drawing.Size(375, 0);
+            this.smoothFlowPanel1.Name = "smoothFlowPanel1";
+            this.smoothFlowPanel1.Size = new System.Drawing.Size(375, 648);
+            this.smoothFlowPanel1.TabIndex = 5;
+            this.smoothFlowPanel1.WrapContents = false;
+            // 
+            // ucItemInBatch1
+            // 
+            this.ucItemInBatch1.AutoSize = true;
+            this.ucItemInBatch1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ucItemInBatch1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ucItemInBatch1.Location = new System.Drawing.Point(3, 3);
+            this.ucItemInBatch1.MinimumSize = new System.Drawing.Size(375, 0);
+            this.ucItemInBatch1.Name = "ucItemInBatch1";
+            this.ucItemInBatch1.Size = new System.Drawing.Size(375, 210);
+            this.ucItemInBatch1.TabIndex = 0;
+            // 
+            // ucItemInBatch2
+            // 
+            this.ucItemInBatch2.AutoSize = true;
+            this.ucItemInBatch2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ucItemInBatch2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ucItemInBatch2.Location = new System.Drawing.Point(3, 219);
+            this.ucItemInBatch2.MinimumSize = new System.Drawing.Size(375, 0);
+            this.ucItemInBatch2.Name = "ucItemInBatch2";
+            this.ucItemInBatch2.Size = new System.Drawing.Size(375, 210);
+            this.ucItemInBatch2.TabIndex = 1;
+            // 
+            // ucItemInBatch3
+            // 
+            this.ucItemInBatch3.AutoSize = true;
+            this.ucItemInBatch3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ucItemInBatch3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ucItemInBatch3.Location = new System.Drawing.Point(3, 435);
+            this.ucItemInBatch3.MinimumSize = new System.Drawing.Size(375, 0);
+            this.ucItemInBatch3.Name = "ucItemInBatch3";
+            this.ucItemInBatch3.Size = new System.Drawing.Size(375, 210);
+            this.ucItemInBatch3.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.panel2.Controls.Add(this.lblTime);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 14);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(375, 62);
+            this.panel2.TabIndex = 4;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTime.Location = new System.Drawing.Point(330, 23);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(16, 21);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(113, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "#1212";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(3, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Table 22";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 14);
+            this.panel1.TabIndex = 3;
+            // 
+            // UCBatchCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.Controls.Add(this.panel3);
+            this.MinimumSize = new System.Drawing.Size(395, 0);
+            this.Name = "UCBatchCard";
+            this.Size = new System.Drawing.Size(395, 724);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.smoothFlowPanel1.ResumeLayout(false);
+            this.smoothFlowPanel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel3;
+        private SmoothFlowPanel smoothFlowPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private UCItemInBatch ucItemInBatch1;
+        private UCItemInBatch ucItemInBatch2;
+        private UCItemInBatch ucItemInBatch3;
+    }
+}
---
+++ LaCucina/UI/UCItemInBatch.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace LaCucina.UI
+{
+    partial class UCItemInBatch
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.smoothFlowPanel1 = new SmoothFlowPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.smoothFlowPanel2 = new SmoothFlowPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label27 = new System.Windows.Forms.Label();
+            this.label28 = new System.Windows.Forms.Label();
+            this.smoothFlowPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.smoothFlowPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // smoothFlowPanel1
+            // 
+            this.smoothFlowPanel1.AutoScroll = true;
+            this.smoothFlowPanel1.AutoSize = true;
+            this.smoothFlowPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.smoothFlowPanel1.Controls.Add(this.label26);
+            this.smoothFlowPanel1.Controls.Add(this.label27);
+            this.smoothFlowPanel1.Controls.Add(this.label28);
+            this.smoothFlowPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.smoothFlowPanel1.Location = new System.Drawing.Point(3, 3);
+            this.smoothFlowPanel1.MinimumSize = new System.Drawing.Size(375, 0);
+            this.smoothFlowPanel1.Name = "smoothFlowPanel1";
+            this.smoothFlowPanel1.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.smoothFlowPanel1.Size = new System.Drawing.Size(375, 67);
+            this.smoothFlowPanel1.TabIndex = 6;
+            this.smoothFlowPanel1.WrapContents = false;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(3, 76);
+            this.panel2.MinimumSize = new System.Drawing.Size(375, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.panel2.Size = new System.Drawing.Size(375, 86);
+            this.panel2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(0, 29);
+            this.label3.MaximumSize = new System.Drawing.Size(375, 0);
+            this.label3.MinimumSize = new System.Drawing.Size(375, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(375, 57);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "i have a garlec alirgy do not add any or it will kill me and my fimily will sue y" +
+    "ou\r\n\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.ForeColor = System.Drawing.Color.IndianRed;
+            this.label4.Location = new System.Drawing.Point(0, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Note";
+            // 
+            // smoothFlowPanel2
+            // 
+            this.smoothFlowPanel2.AutoScroll = true;
+            this.smoothFlowPanel2.AutoSize = true;
+            this.smoothFlowPanel2.Controls.Add(this.smoothFlowPanel1);
+            this.smoothFlowPanel2.Controls.Add(this.panel2);
+            this.smoothFlowPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.smoothFlowPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.smoothFlowPanel2.Location = new System.Drawing.Point(0, 45);
+            this.smoothFlowPanel2.MinimumSize = new System.Drawing.Size(375, 0);
+            this.smoothFlowPanel2.Name = "smoothFlowPanel2";
+            this.smoothFlowPanel2.Size = new System.Drawing.Size(375, 165);
+            this.smoothFlowPanel2.TabIndex = 8;
+            this.smoothFlowPanel2.WrapContents = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(0, 1);
+            this.label2.MaximumSize = new System.Drawing.Size(375, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label2.Size = new System.Drawing.Size(262, 44);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "1×Chicken shaworma";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 1);
+            this.panel1.TabIndex = 0;
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label26.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label26.Location = new System.Drawing.Point(3, 10);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(78, 19);
+            this.label26.TabIndex = 27;
+            this.label26.Text = "no cheess";
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label27.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label27.Location = new System.Drawing.Point(3, 29);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(78, 19);
+            this.label27.TabIndex = 28;
+            this.label27.Text = "no cheess";
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label28.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label28.Location = new System.Drawing.Point(3, 48);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(78, 19);
+            this.label28.TabIndex = 29;
+            this.label28.Text = "no cheess";
+            // 
+            // UCItemInBatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.Controls.Add(this.smoothFlowPanel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(375, 0);
+            this.Name = "UCItemInBatch";
+            this.Size = new System.Drawing.Size(375, 210);
+            this.smoothFlowPanel1.ResumeLayout(false);
+            this.smoothFlowPanel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.smoothFlowPanel2.ResumeLayout(false);
+            this.smoothFlowPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private SmoothFlowPanel smoothFlowPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private SmoothFlowPanel smoothFlowPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.Label label28;
+    }
+}
